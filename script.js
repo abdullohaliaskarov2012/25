c60bbcb0ed7a2a3141b5357fc6851dbfb964ea4a
abdullohaliaskarov2012 <aliaskarovabdulloh@gmail.com>
2024-12-17 04:56:42 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+alert('sizda hozir random sonlar chiqishni boshlaydi😉')
+let realNumber = Math.floor(Math.random() * 10);
+alert(`random orqali chiqqan son: ${realNumber}`)
+alert('biz random sonni 2 ga kopaytirmoqchimiz va bolmoqchimiz!')
+console.log("Tasodifiy son:", realNumber);
+let result = realNumber * 2;
+console.log("Ko'paytirilgan natija:", result);
+let natija = realNumber / 2;
+console.log("bo'lingan natija:", natija);
+
+alert(`random orqali chiqqan son: ${realNumber} edi\n\nuni 2ga ko'paytirsak: ${result} boladi\n\nuni 2ga bolsak ${natija} bo'ladi\n\nuni o'zidan bitta kam songa bo'lsak qoldiq har doim 1 chiqadi`)
